3d130a15310954b45c0969100ac3ca9db55929d1
Zhang, Feng <15110700005@fudan.edu.cn>
2022-11-24 19:21:31 +0800

    Update bed2longrange.sh

--- src/bed2longrange.sh
+++ src/bed2longrange.sh
@@ -1,6 +1,6 @@
 BED=$1
 
-python3 /home/database/data/HiChip_Li/EACH/bed2longrange.py $BED 
+python3 bed2longrange.py $BED 
 bedtools sort -i $BED\.longrange > $BED\.longrange.bed
 bgzip $BED\.longrange.bed
 tabix -p bed $BED\.longrange.bed.gz
